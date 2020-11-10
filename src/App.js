9d71dc0bf917a91f26508856567fb1d783ae9cd3
‘Juliano <julianoperins@gmail.com>
2020-11-10 09:38:52 -0500

    Added the data to util

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 
 // Scss
 import "./styles/app.scss";
 
+//Import Util
+import { data } from "./util";
+
 function App() {
+  const [songs, setSongs] = useState(data);
+  console.log(songs);
+
   return (
     <div className="App">
       <Song />
